4f479d127a58c4905b65814201ab5f819883deab
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-05-29 07:08:02 -0500

    feat: :sparkles: union de funciones autofuncionales

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,11 @@
-(function(nombre){
-    console.log(`Hola ${nombre}`);
-})("Nicolas")
+const saludar = (function(){
+    let num = 0;
+    return function(){
+        num++;
+        return num
+    }
+})()
+
+console.log(saludar());
+console.log(saludar());
+console.log(saludar());
